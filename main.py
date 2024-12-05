cdcb52613e755bb5451b02e43db2796bcd770c6c
Gustavo Voltani von Atzingen <gustavo.von.atzingen@gmail.com>
2024-12-05 16:40:39 -0300

    criado arquivo de html

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello oficina do git")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
